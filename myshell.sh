24eabae5c1189be5f1f31232c07e1da79c94b9a5
viswanathanveee <94613781+viswanathanveee@users.noreply.github.com>
2021-11-22 17:08:10 +0530

    myshellscript

---
+++ myshell.sh
@@ -0,0 +1,8 @@
+echo "My Shell Script"
+expr 5 + 3
+echo "Total Arguments: $#"
+echo "Argument One: $1 , Argument Two: $2 ."
+echo "Exercise 2: Reading Input"
+echo "Type in a string and then press Enter:"
+read response
+echo "You entered: $response"
